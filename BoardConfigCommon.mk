2747bd9ae72c6ed1efed86d7575daa7e708a4d23
ArchieMeng <archiemeng@protonmail.com>
2025-06-05 20:05:01 +0800

    Disable SELinux

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -181,3 +181,4 @@ WPA_SUPPLICANT_VERSION := VER_0_8_X
 
 # Inherit from the proprietary version
 include vendor/motorola/sm6225-common/BoardConfigVendor.mk
+BOARD_BOOTCONFIG += androidboot.selinux=permissive
